b701eada7d066ee7a3e14e6eab94faf98075f4d7
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-19 17:10:26 +0100

    Another test

--- api/api/util.py
+++ api/api/util.py
@@ -49,7 +49,7 @@ def get_file_content(authorization: str = "", content_url: str = "") -> bytes:
         raise InvalidUrlParameterException
 
     response = requests.get(content_url, headers={"authorization": authorization}, timeout=15)
-    print(response)
+    raise HTTPException(status_code=response.status_code)
     if response.status_code == 200:
         return response.content
     if response.status_code == 404:
