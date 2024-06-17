705f49ec021dd365daa1d8dfef91cd342078f194
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-06-17 16:13:07 +0200

    Add trace tests

--- tests/fixtures/nexus.py
+++ tests/fixtures/nexus.py
@@ -13,5 +13,10 @@ def morphology_content_url() -> str:
 
 
 @pytest.fixture
+def trace_content_url() -> str:
+    return "https://bbp.epfl.ch/nexus/v1/files/bbp/mouselight/https%3A%2F%2Fbbp.epfl.ch%2Fnexus%2Fv1%2Fresources%2Fbbp%2Fmouselight%2F_%2F0befd25c-a28a-4916-9a8a-adcd767db118"
+
+
+@pytest.fixture
 def access_token() -> str:
     return ""
--- tests/fixtures/utils.py
+++ tests/fixtures/utils.py
@@ -2,12 +2,26 @@
 Utils module for unit tests
 """
 
+import time
+import h5py
+from pynwb import NWBHDF5IO
 
-def load_content(json_path: str, encoded: bool = True):
+
+def load_content(file_path: str, encoded: bool = True):
     """
     Loads content from a file
     """
-    with open(json_path) as file:
+    with open(file_path) as file:
         if encoded:
             return file.read().encode("utf-8")
         return file.read()
+
+
+def load_nwb_content(file_path: str):
+    """
+    Loads content for an NWB file
+    """
+    with open(file_path, "rb") as file:
+        file_content = file.read()
+
+    return file_content
