49e5cd821732f8db20902f0d833385dcea705324
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-06-18 11:27:01 +0200

    Replace with class

--- tests/test_router/test_generate.py
+++ tests/test_router/test_generate.py
@@ -6,55 +6,61 @@ from tests.fixtures.utils import load_content
 from api.user import User
 
 
-client = TestClient(app)
+class TestMorphologyImage:
+    @classmethod
+    def setup_class(cls):
+        cls.client = TestClient(app)
+        app.dependency_overrides[retrieve_user] = cls.override_retrieve_user
 
+    @staticmethod
+    def override_retrieve_user():
+        return User(access_token="test-access-token", username="test")
 
-def override_retrieve_user():
-    return User(access_token="test-access-token", username="test")
-
-
-app.dependency_overrides[retrieve_user] = override_retrieve_user
-
-
-@patch("api.services.morpho_img.fetch_file_content", return_value=load_content("./tests/fixtures/data/morphology.swc"))
-def test_morphology_thumbnail_generation_returns_200_and_image(fetch_file_content):
-    headers = {"Authorization": "Bearer fake-super-secret-token"}
-    response = client.get(
-        "/generate/morphology-image",
-        headers=headers,
-        params={"content_url": "http://example.com/image", "dpi": 300},
+    @patch(
+        "api.services.morpho_img.fetch_file_content", return_value=load_content("./tests/fixtures/data/morphology.swc")
     )
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "image/png"
+    def test_morphology_thumbnail_generation_returns_200_and_image(self, fetch_file_content):
+        headers = {"Authorization": "Bearer fake-super-secret-token"}
+        response = self.client.get(
+            "/generate/morphology-image",
+            headers=headers,
+            params={"content_url": "http://example.com/image", "dpi": 300},
+        )
+        assert response.status_code == 200
+        assert response.headers["content-type"] == "image/png"
+
+    @patch("requests.get")
+    def test_morphology_thumbnail_generation_returns_404_if_resource_not_exists(self, mock_get):
+        headers = {"Authorization": "Bearer fake-super-secret-token"}
+        mock_response = Mock()
+        mock_response.status_code = 404
+        mock_response.json.return_value = None
+        mock_get.return_value = mock_response
+        response = self.client.get(
+            "/generate/morphology-image",
+            headers=headers,
+            params={"content_url": "http://example.com/image", "dpi": 300},
+        )
+        assert response.status_code == 404
+        assert response.json()["detail"] == "The resource is not found"
+
+    @patch("requests.get")
+    def test_morphology_thumbnail_generation_returns_422_if_content_url_is_wrong(self, mock_get):
+        headers = {"Authorization": "Bearer fake-super-secret-token"}
+        mock_response = Mock()
+        mock_response.status_code = 404
+        mock_response.json.return_value = None
+        mock_get.return_value = mock_response
+        response = self.client.get(
+            "/generate/morphology-image",
+            headers=headers,
+            params={"content_url": "notAurl/image", "dpi": 300},
+        )
+        assert response.status_code == 422
+        assert response.json()["detail"] == "Invalid content_url parameter in request"
 
 
-@patch("requests.get")
-def test_morphology_thumbnail_generation_returns_404_if_resource_not_exists(mock_get):
-    headers = {"Authorization": "Bearer fake-super-secret-token"}
-    mock_response = Mock()
-    mock_response.status_code = 404
-    mock_response.json.return_value = None
-    mock_get.return_value = mock_response
-    response = client.get(
-        "/generate/morphology-image",
-        headers=headers,
-        params={"content_url": "http://example.com/image", "dpi": 300},
-    )
-    assert response.status_code == 404
-    assert response.json()["detail"] == "The resource is not found"
+if __name__ == "__main__":
+    import pytest
 
-
-@patch("requests.get")
-def test_morphology_thumbnail_generation_returns_422_if_content_url_is_wrong(mock_get):
-    headers = {"Authorization": "Bearer fake-super-secret-token"}
-    mock_response = Mock()
-    mock_response.status_code = 404
-    mock_response.json.return_value = None
-    mock_get.return_value = mock_response
-    response = client.get(
-        "/generate/morphology-image",
-        headers=headers,
-        params={"content_url": "notAurl/image", "dpi": 300},
-    )
-    assert response.status_code == 422
-    assert response.json()["detail"] == "Invalid content_url parameter in request"
+    pytest.main()
