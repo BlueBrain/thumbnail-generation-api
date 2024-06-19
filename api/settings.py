568bc01cb5212cd9cebf18c68fc704e5bef67173
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-06-19 15:04:00 +0200

    Fix linter

--- api/settings.py
+++ api/settings.py
@@ -4,7 +4,6 @@ Module to setup the environment variables of the application
 
 import matplotlib
 from dotenv import load_dotenv
-from pydantic import ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from api.models.enums import Environment
 
